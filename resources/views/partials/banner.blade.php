2f97ada1036e1239c1e064f1281721ec8230e8b0
duulat <dulat-kg@mail.ru>
2024-06-11 09:36:55 +0600

    remove comment

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -2,18 +2,15 @@
     <div class="max-w-7xl mx-auto">
         <div class="lg:flex lg:items-center lg:justify-between">
             <div class="lg:w-0 lg:flex-1">
-                <!-- Текст баннера -->
                 <h2 class="text-4xl font-extrabold tracking-tight text-white sm:text-5xl">
                     Получите скидку 20% на все услуги
                 </h2>
                 <p class="mt-3 max-w-3xl text-lg leading-6 text-gray-300">
                     Предложение ограничено. Бронируйте сейчас!
                 </p>
-                <!-- Кнопка -->
-              ч
+
             </div>
             <div class="mt-8 lg:mt-0 lg:ml-8">
-                <!-- Изображение баннера (заглушка) -->
                 <div class="inline-block relative w-full overflow-hidden bg-gray-500 rounded-lg shadow-xl">
                     <img src="https://aqualab.kg/uploads/a/admin/original/18232.jpg" alt="Изображение баннера" width="500[px]" class="relative" />
                 </div>
